08d448939e3e6b22ea3643f125546f847c213ab3
Varsha <vkuppurr@andrew.cmu.edu>
2020-03-20 22:33:24 -0400

    bert+cnn class

--- models/cnn_bert.py
+++ models/cnn_bert.py
@@ -14,17 +14,23 @@ class CNN_BERT(nn.Module):
 
     def __init__(self,out_dim=300,embed_dim=1024,max_len=1000, dropout=0.5):
         super(CNN_BERT, self).__init__()
+        
+        self.embed_dim = embed_dim
+        self.out_dim = out_dim
+        self.max_len = max_len
+        self.dropout = dropout
+
         self.bert_layer = BertModel.from_pretrained('bert-large-uncased')
         self.conv_3 = nn.Conv1d(1, out_dim, embed_size * 3, stride=embed_size)
         self.conv_4 = nn.Conv1d(1, out_dim, embed_size * 4, stride=embed_size)
         self.conv_5 = nn.Conv1d(1, out_dim, embed_size * 5, stride=embed_size)
 
     def get_conv_out(self,conv,x, num):
-        return nn.functional.max_pool1d(nn.functional.relu(conv(x)), 
-                                        max_len - num + 1).view(-1, feature_map)
+        return F.max_pool1d(F.relu(conv(x)), 
+                            self.max_len - num + 1).view(-1, self.out_dim)
 
     def forward(self,x):
-        x = self.bert_layer(x)[0].view(-1, 1, embed_size*max_len)
+        x = self.bert_layer(x)[0].view(-1, 1, self.embed_dim*self.max_len)
         conv_3 = self.get_conv_out(self.conv_3,x,3)
         conv_4 = self.get_conv_out(self.conv_4,x,4)
         conv_5 = self.get_conv_out(self.conv_5,x,5)
